4e56c138a38adae1b05db1087fd721b062800d85
Brooke Luro <lurob@me.com>
2020-04-27 18:20:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,12 +18,10 @@ function ageChecker(age) {
     return "You are a teenager!"
   } else if (age < 13) {
     return "You are a kid"
-  } else (age > 19) {
+  } else (age > 19)
     return "You are a grownup"
   }
-  }
-  }
-}
+  
 
 function ternaryTeenager(age) {
   return (age > 12 && age < 20) ?
